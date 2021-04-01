8873d96be05dec9cdc68116821280a80011fc3b6
aharri64 <haveacompleteday@gmail.com>
2021-04-01 15:43:27 -0600

    signup initialized

--- spacebarapp/accounts/models.py
+++ spacebarapp/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.urls import reverse
+from datetime import date
 # Create your models here.
+from django.contrib.auth.models import User
+
+class User(model.Model, auth.Model):
+
+    def __str__(self):
+        return "@{}".format(self.username)
--- spacebarapp/accounts/views.py
+++ spacebarapp/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.urls import reverse
+from django.views.generic import CreateView
 # Create your views here.
+from . import forms
+
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse('Login')
+    template_name = 'accounts/signup.html'
