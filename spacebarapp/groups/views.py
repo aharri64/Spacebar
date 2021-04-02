c72f9c81fcf2e584006d15942aa7235cb1aaf857
aharri64 <haveacompleteday@gmail.com>
2021-04-02 15:11:10 -0600

    added groups viewsz

--- spacebarapp/groups/views.py
+++ spacebarapp/groups/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+from django.contrib import messages
+from django.db import IntegrityError
+from django.urls import reverse
+from django.views import generic
+from .models import Group, GroupMember
 # Create your views here.
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
