9c06e701d7a1690a004b9452fef1d5c700d13256
aharri64 <haveacompleteday@gmail.com>
2021-04-01 23:59:40 -0600

    views

--- spacebarapp/accounts/views.py
+++ spacebarapp/accounts/views.py
@@ -1,11 +1,14 @@
+
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
-# Create your views here.
+
 from . import forms
+
+# Create your views here.
 
 
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
-    success_url = reverse('Login')
+    success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
