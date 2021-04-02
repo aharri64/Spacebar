90c9c66409d5efc6d1efc52e1554bf7f70e2c191
aharri64 <haveacompleteday@gmail.com>
2021-04-01 23:59:57 -0600

    views

--- spacebarapp/spacebarapp/views.py
+++ spacebarapp/spacebarapp/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
 
 
-def index(request):
-    return render(request, 'index.html')
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+
+class HomePage(TemplateView):
+    template_name = "index.html"
