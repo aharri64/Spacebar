59f53c0dd718c25cbfc19c7ac5d63377d73138ee
aharri64 <haveacompleteday@gmail.com>
2021-04-01 23:59:25 -0600

    models

--- spacebarapp/accounts/models.py
+++ spacebarapp/accounts/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.urls import reverse
-from datetime import date
-# Create your models here.
-from django.contrib.auth.models import User
+from django.contrib import auth
 
-class User(model.Model, auth.Model):
+# Create your models here.
+
+
+class User(auth.models.User, auth.models.PermissionsMixin):
 
     def __str__(self):
         return "@{}".format(self.username)
