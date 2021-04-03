f8f18c738964707785d86d22b7e4fcb518ee13f9
aharri64 <haveacompleteday@gmail.com>
2021-04-02 22:33:17 -0600

    Config deployment

--- spacebarapp/spacebarapp/settings.py
+++ spacebarapp/spacebarapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,11 +89,11 @@ DATABASES = {
     }
 }
 
-# if not DEBUG:
-#     DATABASES["default"] = dj_database_url.config(
-#         conn_max_age=600,
-#         ssl_require=True,
-# )
+if not DEBUG:
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+    )
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
